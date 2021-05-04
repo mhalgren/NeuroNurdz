1d571502038ace73c8892bbf59039dc3d910a7f7
icweaver <iweaver@cfa.harvard.edu>
2021-05-03 22:40:12 -0400

    laggies

--- CC.jl
+++ CC.jl
@@ -1,77 +1,70 @@
 ### A Pluto.jl notebook ###
-# v0.14.5
+# v0.14.4
 
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ b16b5d93-acaf-4440-bdab-2d523ff878b8
-import(Pkg); Pkg.add("PlutoUI")
-
 # ╔═╡ acc935c2-d23d-4e64-bd84-fb96cd67d1ed
 begin
-	### A Pluto.jl notebook ###
-	# v0.14.2
-	
 	using Markdown
-	using InteractiveUtils
+	using PlutoUI
+	using CairoMakie
+	using StatsBase
+	using BenchmarkTools
 end
-
-# ╔═╡ 47c6a4e6-379f-4a78-8249-2431311c4397
-using PlutoUI
-
-# ╔═╡ 1a2168a6-ac78-11eb-3150-7ffde5908665
-
 
 # ╔═╡ 14e2a9a4-f625-433d-9552-a1b9be720bcd
-x = abs.(rand(1,10000)) * 2; y = abs.(rand(1,4000)) * 2;
+xs = rand(0:1e-4:4_000.0, 500); ys = rand(0:1e-4:4_000.0, 600);
 
-# ╔═╡ 3af852e7-dfb7-47e0-adc8-22db644d3e2d
-for i in range(
+# ╔═╡ 0072a0cf-9396-4acf-9a02-a7a2c9cee2fe
+thresh = 50
 
-# ╔═╡ 698bfb46-54f1-4acb-aed0-3f3fbac6c997
-let
-diffsd = zeros(1,500000);
-k = 2
-	tmp=vec(y.-x[k]); 
-	deleteat!(tmp, abs.(tmp).>.05);
-	if k > 1
-	diffsd[ max(findall(diffsd)) : (max(findall(diffsd))+length(tmp).-1)] = tmp;
-		else
-	diffsd[ 1 : (1+length(tmp).-1)] = tmp;
-		end
-    
-    #ci = ci .+ length(tmp);
-
+# ╔═╡ 6daf52cb-21db-4708-aeb0-830281298678
+function lag_push(xs, ys)
+	lags = []
+	for x in xs, y in ys
+		abs(x - y) ≤ thresh && push!(lags, x - y)
+	end
+	
+	return lags
 end
 
-# ╔═╡ 37ddc497-612d-40ea-98aa-fc73c9cb518a
-#with_terminal() do
-begin
-diffsd = zeros(1,500000);#
-	ci = 1
-for k = 1:10#length(x)
-	if k > 1
-	ci = max(findall(diffsd))
-		else
-			ci = 1
-		end
-    tmp=y.-x[k]; tmp[abs.(tmp).>.05] = [];
-    diffsd[ ci : (ci+length(tmp).-1)] = tmp;
-    #ci = ci .+ length(tmp);
-end
-end
+# ╔═╡ 57a438b6-56e6-4260-b423-63889f0e757b
+myhist = fit(Histogram, lag_push(xs, ys),  -50:5:50, closed=:left)
 
+# ╔═╡ 66a38b00-7d1f-4f18-8928-bf23d3710df1
+myhist.weights
 
-# ╔═╡ d283df1c-da1c-4d17-b6dd-a474283755c2
-diffsd2 = diffsd[1:(ci-1)];
+# ╔═╡ 96c8f81c-610e-454f-96f6-3942bd8e7253
+plot(myhist)
+
+# ╔═╡ 1b02b691-c141-47c8-922b-e79fc5c30bd6
+# const length_x = length(xs)
+# const length_y = length(ys)
+
+# ╔═╡ 308fe3b9-398b-4c6a-b13d-e1812972393c
+# function lag_prealloc()
+# 	lags = zeros(Float64, length_x*length_y)
+# 	i = 1
+# 	for x in xs, y in ys
+# 		abs(x - y) ≤ thresh && (lags[i] = abs(x - y))
+# 		i += 1
+# 	end
+	
+# 	return lags
+# end
+
+# ╔═╡ 2d833919-8538-4e88-a6a5-e1a1eca7ea6b
+# filter!(!=(0.0, ), lag_prealloc())
 
 # ╔═╡ Cell order:
-# ╠═1a2168a6-ac78-11eb-3150-7ffde5908665
 # ╠═acc935c2-d23d-4e64-bd84-fb96cd67d1ed
-# ╠═b16b5d93-acaf-4440-bdab-2d523ff878b8
-# ╠═47c6a4e6-379f-4a78-8249-2431311c4397
 # ╠═14e2a9a4-f625-433d-9552-a1b9be720bcd
-# ╠═d283df1c-da1c-4d17-b6dd-a474283755c2
-# ╠═3af852e7-dfb7-47e0-adc8-22db644d3e2d
-# ╠═698bfb46-54f1-4acb-aed0-3f3fbac6c997
-# ╠═37ddc497-612d-40ea-98aa-fc73c9cb518a
+# ╠═0072a0cf-9396-4acf-9a02-a7a2c9cee2fe
+# ╠═6daf52cb-21db-4708-aeb0-830281298678
+# ╠═57a438b6-56e6-4260-b423-63889f0e757b
+# ╠═66a38b00-7d1f-4f18-8928-bf23d3710df1
+# ╠═96c8f81c-610e-454f-96f6-3942bd8e7253
+# ╠═1b02b691-c141-47c8-922b-e79fc5c30bd6
+# ╠═308fe3b9-398b-4c6a-b13d-e1812972393c
+# ╠═2d833919-8538-4e88-a6a5-e1a1eca7ea6b
